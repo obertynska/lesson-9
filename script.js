f77963c2ea8d0e13519ddf31d5067873d286ce46
obertynska <obertynska11@gmail.com>
2020-02-06 09:00:43 +0200

    lesson 9 is done

---
+++ script.js
@@ -0,0 +1,70 @@
+'use strict';
+
+let buttonCalculate = document.getElementById("start");
+console.log(buttonCalculate);
+
+let buttonPlusFirst = document.getElementsByTagName("button")[0];
+console.log(buttonPlusFirst);
+
+let buttonPlusSecond = document.getElementsByTagName("button")[1];
+console.log(buttonPlusSecond);
+
+let checkBox = document.querySelector("#deposit-check");
+console.log(checkBox);
+
+let checkMark = document.querySelector(".deposit-checkmark");
+console.log(checkMark);
+
+let possibleIncomesNames = document.querySelectorAll(".additional_income-item");
+console.log(possibleIncomesNames);
+
+let budgetDay = document.getElementsByClassName("budget_day-value")[0];
+console.log(budgetDay);
+
+let expensesMonth = document.getElementsByClassName("expenses_month-value")[0];
+console.log(expensesMonth);
+
+let addIncomesValues = document.getElementsByClassName("additional_income-value")[0];
+console.log(addIncomesValues);
+
+let addExpensesValues = document.getElementsByClassName("additional_expenses-value")[0];
+console.log(addExpensesValues);
+
+let incomeValueOfPeriod = document.getElementsByClassName("income_period-value")[0];
+console.log(incomeValueOfPeriod);
+
+let targetMonth = document.getElementsByClassName("target_month-value")[0];
+console.log(targetMonth);
+
+let budgetMonth = document.querySelector(".budget_month-value");
+console.log(budgetMonth);
+
+let salary = document.querySelector(".salary-amount");
+console.log(salary);
+
+let addIncomeName = document.querySelectorAll(".income-title")[1];
+console.log(addIncomeName);
+
+let addIncomeNameAmount = document.querySelector(".income-amount");
+console.log(addIncomeNameAmount);
+
+let obligatoryExpensesName = document.querySelectorAll(".expenses-title")[1];
+console.log(obligatoryExpensesName);
+
+let obligatoryExpensesAnount = document.querySelector(".expenses-amount");
+console.log(obligatoryExpensesAnount);
+
+let possibleExpenses = document.querySelector(".additional_expenses-item");
+console.log(possibleExpenses);
+
+
+let targetAmount = document.querySelector(".target-amount");
+console.log(targetAmount);
+
+let rangePeriod = document.querySelector(".period-select");
+console.log(rangePeriod);
+
+
+
+
+
